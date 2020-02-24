ad38463dcce3776b75af019cc3fef10a0bdb356d
Gottskalksson <adam.rolirad@gmail.com>
2020-02-24 22:04:45 +0100

    Added method CRUD

--- src/main/java/pl/raporty/daos/UserDao.java
+++ src/main/java/pl/raporty/daos/UserDao.java
@@ -1,5 +1,13 @@
 package pl.raporty.daos;
 
+import pl.raporty.DBUtil.DbUtil;
+import pl.raporty.models.User;
+
+import javax.ws.rs.NotFoundException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class UserDao {
     private static final String CREATE_USER_QUERY =
             "INSERT INTO users (user_name, password) VALUES (?, ?)";
@@ -9,4 +17,79 @@ public class UserDao {
             "UPDATE users SET user_name = ?, password = ? WHERE user_id = ?";
     private static final String DELETE_USER_QUERY =
             "DELETE FROM users WHERE user_id = ?";
+
+    public User create (User user) {
+        try (Connection conn = DbUtil.getConnection();
+             PreparedStatement statement = conn.prepareStatement(CREATE_USER_QUERY,
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getPassword());
+            int result = statement.executeUpdate();
+
+            if (result != 1) {
+                throw new RuntimeException("Execute update returned " + result);
+            }
+
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.first()) {
+                    user.setId(generatedKeys.getInt(1));
+                    return user;
+                } else {
+                    throw new RuntimeException("Generated kay was now found");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void delete(Integer userId) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(DELETE_USER_QUERY)) {
+            statement.setInt(1, userId);
+            statement.executeUpdate();
+
+            boolean deleted = statement.execute();
+            if (!deleted) {
+                throw new NotFoundException("Person not found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update (User user) {
+        try (Connection conn = DbUtil.getConnection();
+        PreparedStatement statement = conn.prepareStatement(UPDATE_USER_QUERY)) {
+            statement.setInt(3, user.getId());
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getPassword());
+
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User read(Integer userId) {
+        User user = new User();
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(READ_USER_QUERY);
+        ) {
+            statement.setInt(1, userId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    user.setId(resultSet.getInt("user_id"));
+                    user.setName("user_name");
+                    user.setPassword("password");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+
 }
