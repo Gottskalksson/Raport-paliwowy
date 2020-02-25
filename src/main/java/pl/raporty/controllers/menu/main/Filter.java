286735bde92dc71fdc20e5e245f339f0c03f9b89
Gottskalksson <adam.rolirad@gmail.com>
2020-02-25 15:58:08 +0100

    Filtred added

--- src/main/java/pl/raporty/controllers/menu/main/Filter.java
+++ src/main/java/pl/raporty/controllers/menu/main/Filter.java
@@ -1,13 +1,26 @@
-@javax.servlet.annotation.WebFilter(filterName = "Filter")
+package pl.raporty.controllers.menu.main;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(value = "/app/*")
 public class Filter implements javax.servlet.Filter {
     public void destroy() {
     }
 
-    public void doFilter(javax.servlet.ServletRequest req, javax.servlet.ServletResponse resp, javax.servlet.FilterChain chain) throws javax.servlet.ServletException, java.io.IOException {
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpSession session = request.getSession();
+        if (session.getAttribute("login") == null) {
+            request.getRequestDispatcher("/login").forward(req, resp);
+        }
         chain.doFilter(req, resp);
     }
 
-    public void init(javax.servlet.FilterConfig config) throws javax.servlet.ServletException {
+    public void init(FilterConfig config) throws ServletException {
 
     }
 
